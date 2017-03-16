5f658d90c97ff27b5c0a161c73b3c71005fb8e99
cwade <courtneywade@gmail.com>
2017-03-15 20:49:02 -0400

    Added method to execute an arbitrary command and to insert everything in a data frame into a db table.

--- oracle_db_query/oracle_db_query.py
+++ oracle_db_query/oracle_db_query.py
@@ -5,23 +5,53 @@ import getpass
 import yaml
 from pandas import DataFrame
 
+def open_connection(config_file):
+    with open(config_file, 'r') as ymlfile:
+        cfg = yaml.load(ymlfile)
+
+    user = cfg['database']['username']
+    if 'password' in cfg['database']:
+            pwd = cfg['database']['password']
+    else:
+            pwd = getpass.getpass('Database password: ')
+    host = cfg['database']['host']
+    return(cx_Oracle.connect("{}/{}@{}".format(user, pwd, host)))
+
 def query_to_df(query, config_file):
-	with open(config_file, 'r') as ymlfile:
-	    cfg = yaml.load(ymlfile)
+    con = open_connection(config_file)
+    cur = con.cursor()
+    cur.execute(query)
+    r = cur.fetchall()
+    cols = [n[0] for n in cur.description]
+    cur.close()
+    con.close()
+    data = DataFrame.from_records(r, columns=cols)
+    return(data)
 
-	user = cfg['database']['username']
-	if 'password' in cfg['database']:
-		pwd = cfg['database']['password']
-	else:
-		pwd = getpass.getpass('Database password: ')
-	host = cfg['database']['host']
+def execute(statement, config_file):
+    con = open_connection(config_file)
+    cur = con.cursor()
+    cur.execute(statement)
+    con.commit()
+    cur.close()
+    con.close()
 
-	con = cx_Oracle.connect("{}/{}@{}".format(user, pwd, host))
-	cur = con.cursor()
-	cur.execute(query)
-	r = cur.fetchall()
-	cols = [n[0] for n in cur.description]
-	cur.close()
-	con.close()
-	data = DataFrame.from_records(r, columns=cols)
-	return(data)
+def insert_multiple(table_name, df, config_file, batch_size=5000, print_insert_statements=False):
+    con = open_connection(config_file)
+    cur = con.cursor()
+    sql = "INSERT INTO {0} ({1}) VALUES (:{2})".format(table_name,
+                                                      ', '.join(df.columns),
+                                                      ', :'.join(list(map(str,range(1, len(df.columns)+1)))))
+    i = 0
+    while ((i * batch_size) < len(df)):
+        rows = []
+        min = i*batch_size
+        max = ((i+1)*batch_size)-1
+        for x in df.ix[min:max,:].values:
+            rows.append([None if pd.isnull(y) else y for y in x])
+        cur.executemany(sql, rows)
+        con.commit()
+        i = i + 1
+    cur.close()
+    con.close()
+
