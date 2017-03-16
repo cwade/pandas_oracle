9bace31f8465a115558608703b74d883e80dba27
cwade <courtneywade@gmail.com>
2017-03-15 21:36:59 -0400

    Fixd bug and incremented version

--- pandas_oracle/tools.py
+++ pandas_oracle/tools.py
@@ -3,7 +3,7 @@
 import cx_Oracle
 import getpass
 import yaml
-from pandas import DataFrame
+import pandas as pd
 
 def open_connection(config_file):
     with open(config_file, 'r') as ymlfile:
@@ -25,7 +25,7 @@ def query_to_df(query, config_file):
     cols = [n[0] for n in cur.description]
     cur.close()
     con.close()
-    data = DataFrame.from_records(r, columns=cols)
+    data = pd.DataFrame.from_records(r, columns=cols)
     return(data)
 
 def execute(statement, config_file):
